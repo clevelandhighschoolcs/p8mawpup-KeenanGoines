7def25c703c01a78ce6d30bc6009bcfa0d8cf7b0
KeenanGoines <33072576+KeenanGoines@users.noreply.github.com>
2017-12-14 14:06:39 -0800

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,28 @@
+import urllib2
+
+import time
+
+
+
+def webdata():
+
+	timeT = 0
+
+	length = ' '
+
+	while timeT < 5:
+
+		URL = urllib2.urlopen ('https://twitter.com/YoYoGames?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor')
+		data = URL.read()
+		datastring = str(data)
+		lengthA = len(datastring)
+		
+		print (lengthA)
+		if lengthB != ' ' amd lengthA != lengthB:
+			print ('This tweeter feed has changed')
+			return
+		else:
+			lengthB - lengthA
+		timeT +- 1
+		time.sleep(5)
+webdata()
