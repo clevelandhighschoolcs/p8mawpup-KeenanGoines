6057229a552e92870fa2a542af87ba233d30c144
Victor Todea <victorwtodea@gmail.com>
2017-12-15 16:54:37 -0800

    implemented twilio client - I tested it and it works

--- scrape.py
+++ scrape.py
@@ -1,14 +1,26 @@
 import urllib2
-
 import time
+import re
+import requests
+from twilio.rest import Client
 
+#To ge this to work, do these following things while in your virtualenv scripts folder, whilst having virtualenv enabled:
+
+#pip install twilio
+#pip install Client
+#pip install requests
+
+account_sid = 'your account sid here'
+auth_token = 'your auth token here'
+twilio_phone_number = '+your twilio phone number here'
+my_phone_number = '+your phone number here'
 
 
 def webdata():
 
 	timeT = 0
 
-	length = ' '
+	lengthB = ''
 
 	while timeT < 5:
 
@@ -18,11 +30,18 @@ def webdata():
 		lengthA = len(datastring)
 		
 		print (lengthA)
-		if lengthB != ' ' amd lengthA != lengthB:
-			print ('This tweeter feed has changed')
+		if lengthB != '' and lengthA != lengthB:
+			body = "This tweeter feed has changed!"
+			client = Client(account_sid, auth_token)
+			client.messages.create(
+			body=body,
+			to=my_phone_number,
+			from_=twilio_phone_number
+			)
 			return
 		else:
-			lengthB - lengthA
+			lengthB = lengthA
 		timeT +- 1
 		time.sleep(5)
+		print "Restarting"
 webdata()
